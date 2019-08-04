d49b8443fb9111da03be366ac021fa0849792f69
邓天骄 <jiao_snow@126.com>
2019-08-05 07:34:45 +0800

    feign上传文件

---
+++ config-client/src/main/java/com/learn/configclient/ConfigClientApplication.java
@@ -0,0 +1,17 @@
+package com.learn.configclient;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author Jerry
+ * @Date 2019-08-05 07:21
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigClientApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ConfigClientApplication.class).web(true).run(args);
+    }
+}
