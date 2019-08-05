5a050594dbf56cfa83273ec7adbd935940e3db55
邓天骄 <jiao_snow@126.com>
2019-08-05 23:11:53 +0800

    配置中心-eureka高可用

--- config-client-eureka/src/main/java/com/learn/configclienteureka/contorller/YmlController.java
+++ config-client-eureka/src/main/java/com/learn/configclienteureka/contorller/YmlController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * despaction
+ * 这里需要添加RefreshScope注解为了修复1.5.4.RELEASE版本的springboot不会自动刷新的问题
+ * 但是在1.3.7版本是可以的。这个需要研究下为什么
  *
  * @Author: jerry
  * @date: 2019/8/5 16:39
