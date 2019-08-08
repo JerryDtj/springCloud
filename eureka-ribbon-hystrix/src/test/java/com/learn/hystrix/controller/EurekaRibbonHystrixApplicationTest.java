b23ae9f9cf8997ae87acf42d08806f52cedc1786
邓天骄 <jiao_snow@126.com>
2019-08-08 13:56:42 +0800

    hytrix

---
+++ eureka-ribbon-hystrix/src/test/java/com/learn/hystrix/controller/EurekaRibbonHystrixApplicationTest.java
@@ -0,0 +1,23 @@
+package com.learn.hystrix.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * despaction
+ *
+ * @Author: jerry
+ * @date: 2019/8/8 13:43
+ * @description:
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootApplication
+public class EurekaRibbonHystrixApplicationTest {
+
+    @Test
+    public void contextLoad(){
+
+    }
+}
