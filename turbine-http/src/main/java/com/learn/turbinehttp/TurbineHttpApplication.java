90745e9a8d2be2770170367f64394cf3c1501838
邓天骄 <jiao_snow@126.com>
2019-08-14 16:23:09 +0800

    turbint-http

---
+++ turbine-http/src/main/java/com/learn/turbinehttp/TurbineHttpApplication.java
@@ -0,0 +1,15 @@
+package com.learn.turbinehttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jerry
+ * @Date 2019-08-14 16:17
+ */
+@SpringBootApplication
+public class TurbineHttpApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TurbineHttpApplication.class, args);
+    }
+}
---
+++ turbine-http/src/test/java/TurbineHttpApplicationTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Jerry
+ * @Date 2019-08-14 16:21
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TurbineHttpApplicationTest {
+
+    @Test
+    public void context() {
+
+    }
+}
